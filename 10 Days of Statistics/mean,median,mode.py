a26f851f3aa122ef1f37cdb82bfc6b0c213c7933
PRCodeTechnoz <54944535+patilparas@users.noreply.github.com>
2020-02-19 14:05:45 +0530

    Update mean,median,mode.py

--- 10 Days of Statistics/mean,median,mode.py
+++ 10 Days of Statistics/mean,median,mode.py
@@ -1,4 +1,4 @@
-import statistics
+from scipy import stats
 import numpy
 
 n = int(input().strip())
@@ -6,18 +6,23 @@ arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 print(numpy.mean(arr))
 print(numpy.median(arr))
 
-def mode(arr):
-    list_table = statistics._counts(arr)
-    len_table = len(list_table)
+#method 1
+print(int(stats.mode(arr)[0]))
 
-    if len_table == 1:
-        mmode = statistics.mode(arr)
-    else:
-        new_list = []
-        for i in range(len_table):
-            new_list.append(list_table[i][0])
-        mmode = min(new_list) 
-    return mmode
 
-if __name__ == '__main__':
-    print(mode(arr))
+#method 2
+# def mode(arr):
+#     list_table = statistics._counts(arr)
+#     len_table = len(list_table)
+
+#     if len_table == 1:
+#         mmode = statistics.mode(arr)
+#     else:
+#         new_list = []
+#         for i in range(len_table):
+#             new_list.append(list_table[i][0])
+#         mmode = min(new_list) 
+#     return mmode
+
+# if __name__ == '__main__':
+#     print(mode(arr))
