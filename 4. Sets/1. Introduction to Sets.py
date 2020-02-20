d97d99bb725c58fad1e014afaab854354cca3bb2
PRCodeTechnoz <54944535+patilparas@users.noreply.github.com>
2020-02-20 17:39:42 +0530

    Delete 1. Introduction to Sets.py

--- 4. Sets/1. Introduction to Sets.py
+++
@@ -1,9 +0,0 @@
-def average(array):
-    height = set(array)
-    return sum(height) / len(height)
-
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    result = average(arr)
-    print(result)
