7e3cbca19f1d479333348f8e3e419d2f80056e39
PRCodeTechnoz <54944535+patilparas@users.noreply.github.com>
2020-02-20 15:15:41 +0530

    Delete Weighted Mean.py

--- 10 Days of Statistics/Day-0/Weighted Mean.py
+++
@@ -1,8 +0,0 @@
-n = int(input().strip())
-x = list(map(int, input().split()))
-w = list(map(int,input().split()))
-
-wmean = 0
-for i in range(n):
-    wmean = wmean + (x[i] * w[i])
-print(round(wmean/sum(w), 1))
